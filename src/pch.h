89e4605d478340e903457bb64d3ef6b853352a97
Alberto <albertomcaa2005amc3@gmail.com>
2026-02-12 19:19:06 +0000

    Add support for Precompiled Headers (PCH) and CMakeList

---
+++ src/pch.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+#include <iostream>
+
