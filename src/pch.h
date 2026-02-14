00e3b6743451525fcff7aa62306f9cf2ce247407
Alberto <albertomcaa2005amc3@gmail.com>
2026-02-14 21:16:15 +0000

    Organize PCH (Precompiled Headers) file by categories

--- src/pch.h
+++ src/pch.h
@@ -1,12 +1,24 @@
 #pragma once
 
-#include <string>
+// Containers
+#include <array>
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
-#include <memory>
 #include <utility>
+
+// Algorithms and utilities
 #include <algorithm>
 #include <functional>
-#include <iostream>
+#include <memory> // smart pointers
+#include <string>
+#include <string_view>
+#include <sstream>
 
+// I/O
+#include <iostream>
+#include <fstream>
+#include <format> // C++20 formatting
+
+// Project headers
+#include <Engine/Core.h>
