e462e997e9bd6808ecc40818908ff578f6cdfc8c
Alberto <albertomcaa2005amc3@gmail.com>
2026-02-14 21:23:05 +0000

    Extend event system with window, keyboard, and mouse events

---
+++ include/Engine/ApplicationEvent.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "Core.h"
+#include "Event.h"
+#include <sstream> // For std::stringstream
+
+namespace VoxelEngine
+{
+
+    class ENGINE_API WindowResizeEvent : public Event
+    {
+    public:
+        WindowResizeEvent(unsigned int width, unsigned int height)
+            : m_Width(width), m_Height(height) {}
+
+        inline unsigned int GetWidth() const { return m_Width; }
+        inline unsigned int GetHeight() const { return m_Height; }
+
+        std::string ToString() const override
+        {
+            std::stringstream ss;
+            ss << "WindowResizeEvent: " << m_Width << ", " << m_Height;
+            return ss.str();
+        }
+
+        EVENT_CLASS_TYPE(WindowResize)
+        EVENT_CLASS_CATEGORY(EventCategoryApplication)
+    private:
+        unsigned int m_Width, m_Height;
+    };
+
+    class ENGINE_API WindowCloseEvent : public Event
+    {
+    public:
+        WindowCloseEvent() = default;
+
+        EVENT_CLASS_TYPE(WindowClose)
+        EVENT_CLASS_CATEGORY(EventCategoryApplication)
+    };
+
+}
