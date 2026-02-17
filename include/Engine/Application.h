806c3323d4f95fab2733891eb1b9800079430168
Alberto <albertomcaa2005amc3@gmail.com>
2026-02-17 17:27:33 +0000

    Replace includes with forward declarations

--- include/Engine/Application.h
+++ include/Engine/Application.h
@@ -1,15 +1,39 @@
 #pragma once
 #include "Core.h"
+#include <memory>
 
 namespace VoxelEngine
 {
+    class Event;
+    class LayerStack;
+    class Layer;
+    class Window;
+    class WindowCloseEvent;
+
     class ENGINE_API Application
     {
     public:
         Application();
         virtual ~Application();
 
+        void PushLayer(std::unique_ptr<Layer> layer);
+        void PushOverlay(std::unique_ptr<Layer> overlay);
+
+        inline static Application &Get() { return *s_Instance; }
+        inline Window &GetWindow() { return *m_Window; }
+
         void Run();
+
+    private:
+        // Event handling
+        void OnEvent(Event &e);
+        bool OnWindowClose(WindowCloseEvent &e);
+
+    private:
+        std::unique_ptr<class Window> m_Window;
+        std::unique_ptr<LayerStack> m_LayerStack;
+        static Application *s_Instance;
+        bool m_Running = true;
     };
 
     // To be defined in client
--- src/Core/Application.cpp
+++ src/Core/Application.cpp
@@ -1,15 +1,88 @@
-#include <Engine/Application.h>
 #include "pch.h"
+#include <Engine/Layer.h>
+#include <Engine/Application.h>
+#include <Engine/ApplicationEvent.h>
+#include <Engine/Window.h>
+#include <Engine/EventDispatcher.h>
+#include <Engine/Log.h>
+#include "Platform/WindowImpl.h"
+#include "Layers/LayerStack.h"
 
-VoxelEngine::Application::Application()
+namespace VoxelEngine
 {
-}
+    Application *Application::s_Instance = nullptr;
 
-VoxelEngine::Application::~Application()
-{
-}
+    Application::Application()
+    {
+        ENGINE_CORE_ASSERT(!s_Instance, "Application already exists!");
+        s_Instance = this;
 
-void VoxelEngine::Application::Run()
-{
-    std::cout << "Running application..." << std::endl;
+        VoxelEngine::Log::Init();
+
+        // Initialize the layer stack
+        m_LayerStack = std::make_unique<LayerStack>();
+
+        // TODO: Replace this with a CLI, NativeWindow request factory maker
+        // Create a window
+        std::string Title = "Voxel Engine";
+        uint32_t Width = 1280;
+        uint32_t Height = 720;
+        bool Fullscreen = false;
+        bool VSync = true;
+        bool Resizable = true;
+        bool Visible = true;
+        WindowProps props(Title, Width, Height, Fullscreen, VSync, Resizable, Visible);
+
+        m_Window = std::make_unique<VoxelEngine::Window>(props);
+        m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
+    }
+
+    Application::~Application()
+    {
+    }
+
+    void Application::PushLayer(std::unique_ptr<Layer> layer)
+    {
+        Layer *raw = m_LayerStack->PushLayer(std::move(layer));
+        raw->OnAttach();
+    }
+
+    void Application::PushOverlay(std::unique_ptr<Layer> overlay)
+    {
+        Layer *raw = m_LayerStack->PushOverlay(std::move(overlay));
+        raw->OnAttach();
+    }
+
+    void Application::Run()
+    {
+        ENGINE_CORE_TRACE("Application started.");
+
+        // Main loop
+        while (m_Running)
+        {
+            m_Window->OnUpdate();
+        }
+    }
+
+    void Application::OnEvent(Event &e)
+    {
+        EventDispatcher dispatcher(e);
+        dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(Application::OnWindowClose));
+
+        ENGINE_CORE_ASSERT(m_LayerStack, "LayerStack is null!");
+
+        for (auto it = m_LayerStack->rbegin(); it != m_LayerStack->rend(); ++it)
+        {
+            if (e.Handled)
+                break;
+            (*it)->OnEvent(e);
+        }
+    }
+
+    bool Application::OnWindowClose(WindowCloseEvent &e)
+    {
+        m_Running = false;
+        return true;
+    }
+
 }
