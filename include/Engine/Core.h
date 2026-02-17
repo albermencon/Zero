eb516d42a623700cdbdac2b22fea7b0f016e8388
Alberto <albertomcaa2005amc3@gmail.com>
2026-02-17 17:12:04 +0000

    Add core engine assert macros

--- include/Engine/Core.h
+++ include/Engine/Core.h
@@ -3,3 +3,17 @@
 #include "Platform.h"
 #include "Export.h"
 
+#ifdef ENGINE_DEBUG
+    #define ENGINE_ENABLE_ASSERTS
+#endif
+
+#define ENGINE_ENABLE_ASSERTS
+
+#ifdef ENGINE_ENABLE_ASSERTS
+    #define ENGINE_ASSERT(x, ...) { if(!(x)) { ENGINE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+    #define ENGINE_CORE_ASSERT(x, ...) { if(!(x)) { ENGINE_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#else
+    #define ENGINE_ASSERT(x, ...)
+    #define ENGINE_CORE_ASSERT(x, ...)
+#endif
+
