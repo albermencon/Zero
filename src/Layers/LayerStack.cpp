19c0ea9184dfaf226a78c30679af1f08e92b0db1
Alberto <albertomcaa2005amc3@gmail.com>
2026-02-17 17:24:55 +0000

    Add LayerStack

---
+++ src/Layers/LayerStack.cpp
@@ -0,0 +1,48 @@
+#include "pch.h"
+#include "LayerStack.h"
+#include <algorithm>
+#include <memory>
+
+namespace VoxelEngine
+{
+    LayerStack::LayerStack() = default;
+
+    LayerStack::~LayerStack()
+    {
+        for (auto &layer : m_Layers)
+        {
+            layer->OnDetach();
+        }
+    }
+
+    Layer *LayerStack::PushLayer(std::unique_ptr<Layer> layer)
+    {
+        Layer *raw = layer.get();
+        m_Layers.emplace(m_Layers.begin() + m_LayerInsertIndex, std::move(layer));
+        m_LayerInsertIndex++;
+        return raw;
+    }
+
+    Layer *LayerStack::PushOverlay(std::unique_ptr<Layer> overlay)
+    {
+        Layer *raw = overlay.get();
+        m_Layers.emplace_back(std::move(overlay));
+        return raw;
+    }
+
+    void LayerStack::PopLayer(Layer *layer)
+    {
+        auto it = std::find_if(m_Layers.begin(), m_Layers.begin() + m_LayerInsertIndex,
+                               [layer](const std::unique_ptr<Layer> &ptr)
+                               {
+                                   return ptr.get() == layer;
+                               });
+
+        if (it != m_Layers.begin() + m_LayerInsertIndex)
+        {
+            (*it)->OnDetach();
+            m_Layers.erase(it);
+            m_LayerInsertIndex--;
+        }
+    }
+}
---
+++ src/Layers/LayerStack.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <Engine/Layer.h>
+#include <memory>
+#include <vector>
+
+namespace VoxelEngine
+{
+    class LayerStack
+    {
+    public:
+        LayerStack();
+        ~LayerStack();
+
+        Layer *PushLayer(std::unique_ptr<Layer> layer);
+        Layer *PushOverlay(std::unique_ptr<Layer> overlay);
+
+        void PopLayer(Layer *layer);
+
+        // Iterators
+        auto begin() { return m_Layers.begin(); }
+        auto end() { return m_Layers.end(); }
+        auto rbegin() { return m_Layers.rbegin(); }
+        auto rend() { return m_Layers.rend(); }
+
+        auto begin() const { return m_Layers.begin(); }
+        auto end() const { return m_Layers.end(); }
+        auto rbegin() const { return m_Layers.rbegin(); }
+        auto rend() const { return m_Layers.rend(); }
+
+    private:
+        std::vector<std::unique_ptr<Layer>> m_Layers;
+        unsigned int m_LayerInsertIndex = 0;
+    };
+}
